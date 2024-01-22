bfad0ada83bf161f7a4be360000f0d62c3138646
essential2189 <essential2189@gmail.com>
2024-01-23 01:18:04 +0900

    fix: call super annotation

--- src/main/java/com/todaypopup/todaypopup/domain/popup/dto/GetPopupsRequestDto.java
+++ src/main/java/com/todaypopup/todaypopup/domain/popup/dto/GetPopupsRequestDto.java
@@ -2,10 +2,12 @@ package com.todaypopup.todaypopup.domain.popup.dto;
 
 import com.todaypopup.todaypopup.core.dto.PaginationQueryDto;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Data
+@EqualsAndHashCode(callSuper = true)
 @SuperBuilder
 @NoArgsConstructor
 public class GetPopupsRequestDto extends PaginationQueryDto {
--- src/main/java/com/todaypopup/todaypopup/domain/region/dto/GetRegionInfoRequestDto.java
+++ src/main/java/com/todaypopup/todaypopup/domain/region/dto/GetRegionInfoRequestDto.java
@@ -2,10 +2,8 @@ package com.todaypopup.todaypopup.domain.region.dto;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.experimental.SuperBuilder;
 
 @Data
-@SuperBuilder
 @NoArgsConstructor
 public class GetRegionInfoRequestDto {
 
