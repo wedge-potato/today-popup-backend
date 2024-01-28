87ac3d98dc748a377df2308704bbc44270065ae6
essential2189 <essential2189@gmail.com>
2024-01-28 19:16:51 +0900

    fix: EnableWebMvc

--- src/main/java/com/todaypopup/todaypopup/TodayPopupApplication.java
+++ src/main/java/com/todaypopup/todaypopup/TodayPopupApplication.java
@@ -2,10 +2,8 @@ package com.todaypopup.todaypopup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-@EnableWebMvc
 public class TodayPopupApplication {
     public static void main(String[] args) {
         SpringApplication.run(TodayPopupApplication.class, args);
