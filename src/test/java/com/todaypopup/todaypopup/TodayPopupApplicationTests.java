cda16f05c5602bee43017c91e2ac2bb146e576b2
essential2189 <essential2189@gmail.com>
2024-02-04 23:34:09 +0900

    chore: build

--- src/test/java/com/todaypopup/todaypopup/TodayPopupApplicationTests.java
+++ src/test/java/com/todaypopup/todaypopup/TodayPopupApplicationTests.java
@@ -1,13 +1,9 @@
 package com.todaypopup.todaypopup;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TodayPopupApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
 
 }
