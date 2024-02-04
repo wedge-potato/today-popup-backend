374a93f20a6d5de6b5ae667b1ae854c0a779d6f2
왕승재 <essential2189@gmail.com>
2024-02-05 00:27:33 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -16,5 +16,7 @@ cp $ROOT_PATH/build/libs/todaypopup-0.0.1.jar $JAR
 echo "[$NOW] > $JAR 실행" >> $START_LOG
 nohup java -jar -Dspring.profiles.active=$PROFILE $JAR > $APP_LOG 2> $ERROR_LOG &
 
+sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080
+
 SERVICE_PID=$(pgrep -f $JAR)
 echo "[$NOW] > 서비스 PID: $SERVICE_PID" >> $START_LOG
